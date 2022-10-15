9970bfa0501e4af1d08b0b0f5fa975766d04186e
Don Gumayagay <dngumayagay@gmail.com>
2022-10-15 14:42:58 +0800

    added firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from 'firebase/firestore';
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyA-Hy645qSXZe3zDPczIuDTxo0nbID5iR4',
+	authDomain: 'fordacaps.firebaseapp.com',
+	projectId: 'fordacaps',
+	storageBucket: 'fordacaps.appspot.com',
+	messagingSenderId: '488670587196',
+	appId: '1:488670587196:web:90c99555aaa5a86ed79639'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
