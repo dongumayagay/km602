7313c6b42a343fd4f97c61b655cb7182561c70be
Don Gumayagay <dngumayagay@gmail.com>
2022-10-15 14:52:13 +0800

    added ui basic ui for admin

--- src/routes/(admin)/admin/+page.svelte
+++ src/routes/(admin)/admin/+page.svelte
@@ -1,1 +1,9 @@
-this is admin page
+<script>
+	import Admin from './Admin.svelte';
+	import Sign from './Sign.svelte';
+</script>
+
+<h1>this is admin page</h1>
+
+<!-- <Sign /> -->
+<!-- <Admin /> -->
---
+++ src/routes/(admin)/admin/Sign.svelte
@@ -0,0 +1,5 @@
+<form class="flex flex-col gap-2">
+	<input type="email" name="email" placeholder="email" id="" />
+	<input type="password" name="password" placeholder="password" id="" />
+	<button>SIGNIN</button>
+</form>
