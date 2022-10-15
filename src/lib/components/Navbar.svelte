a766ee4d9efecb235b95b55eb38cbd0819fca479
neko-li <erica.flores0307@gmail.com>
2022-10-15 22:27:47 +0800

    change home and booking interface

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,6 +1,20 @@
-<div class="navbar bg-base-100">
+<!-- <div class="navbar bg-base-100">
 	<a href="/" class="btn btn-ghost normal-case text-xl">KM602</a>
 	<div class="ml-auto">
 		<a href="/services">Services</a>
 	</div>
+</div> -->
+
+
+<div class="navbar bg-gray-900 text-white sticky top-0 z-50 drop-shadow-xl">
+	<div class="flex-1 px-2 mx-2 text-blue-500 font-medium text-lg">
+	  <a href="/" class="normal-case"><span class="text-yellow-400 pr-2">km602</span>Carwash</a>
+	</div>
+	<div class="flex-none">
+	  <ul class="menu menu-horizontal p-0">
+		<li><a class="active:bg-transparent active:text-yellow-300 hover:text-yellow-300 hover:bg-transparent" href="/">Home</a></li>
+		<li><a href="/services" class="active:bg-transparent active:text-yellow-300 hover:text-yellow-300 hover:bg-transparent">Services</a></li>
+	  </ul>
+	</div>
 </div>
+
--- src/routes/(client)/+page.svelte
+++ src/routes/(client)/+page.svelte
@@ -2,8 +2,24 @@
 	<title>Home | km602</title>
 </svelte:head>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<a href="/book">Book NOW</a>
+<div class="hero min-h-screen bg-fixed" style="background-image: url(https://images.unsplash.com/photo-1527581849771-416a9d62308e?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=870&amp;q=80);">
+    <div class="hero-overlay bg-gray-900 bg-opacity-50"></div>
+    <div class="hero-content text-center text-neutral-content lg:text-left tracking-wide lg:mr-auto lg:ml-16">
+        <div class="max-w-md">
+            <h1 class="mb-5 text-5xl font-bold">CLEAN CARS RUN BETTER</h1>
 
+            <p class="mb-5">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+                exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+            </p>
+
+            <a href="/book" class="btn btn-warning hover:bg-yellow-500">Book Now</a>
+        </div>
+
+
+    </div>
+</div>
+
+
+<div class="h-screen bg-white"></div>
 
