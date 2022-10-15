cc57ee100890411bc8128ecb943db018c79a7373
Don Gumayagay <dngumayagay@gmail.com>
2022-10-15 15:20:22 +0800

    table ui for bookings done

--- src/routes/(admin)/admin/Admin.svelte
+++ src/routes/(admin)/admin/Admin.svelte
@@ -1,6 +1,8 @@
 <script>
 	import { signOut } from 'firebase/auth';
 	import { auth } from '$lib/firebase';
+	import BookingTable from './BookingTable.svelte';
 </script>
 
 <button on:click={() => signOut(auth)}> LOGOUT </button>
+<BookingTable />
---
+++ src/routes/(admin)/admin/BookingTable.svelte
@@ -0,0 +1,24 @@
+<div class="overflow-x-auto">
+	<table class="table w-full">
+		<!-- head -->
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>Name</th>
+				<th>EMAIL</th>
+				<th>PACKAGE</th>
+				<th>DATETIME</th>
+			</tr>
+		</thead>
+		<tbody>
+			<!-- row 1 -->
+			<tr>
+				<th>123</th>
+				<td>Cy Ganderton</td>
+				<td>cyg@gmail.com</td>
+				<td>PACKAGE 1</td>
+				<td>ngayon</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
