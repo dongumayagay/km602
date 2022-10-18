d8aab2a42b4363e6f0e832dd4e6c69bd544e58b0
Don Gumayagay <dngumayagay@gmail.com>
2022-10-19 07:10:08 +0800

    save user to store

--- src/routes/(admin)/+layout.svelte
+++ src/routes/(admin)/+layout.svelte
@@ -3,11 +3,14 @@
 	import { auth } from '$lib/firebase';
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
+	import { user } from '$lib/stores';
 
 	let loading = true;
+
 	onMount(() => {
-		const unsubscribe = onAuthStateChanged(auth, async (user) => {
-			if (!user) await goto('/admin/signin');
+		const unsubscribe = onAuthStateChanged(auth, async (userObj) => {
+			if (!userObj) await goto('/admin/signin');
+			$user = userObj;
 			loading = false;
 		});
 		return () => unsubscribe();
