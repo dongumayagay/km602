01bd90bd9a2e2cd5bc65a218eeb71c0adb46810c
Don Gumayagay <dngumayagay@gmail.com>
2022-10-19 07:09:20 +0800

    addshorcut to admin

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,33 +1,34 @@
 <footer class="footer p-10 bg-base-100 text-base-content">
-    <div>
-      <span class="footer-title">About Us</span> 
-      <a href="/" class="link link-hover">Branding</a> 
-      <a href="/" class="link link-hover">Design</a> 
-      <a href="/" class="link link-hover">Marketing</a> 
-      <a href="/" class="link link-hover">Advertisement</a>
-    </div> 
-    <div>
-      <span class="footer-title">Our Services</span> 
-      <a href="/" class="link link-hover">About us</a> 
-      <a href="/" class="link link-hover">Contact</a> 
-      <a href="/" class="link link-hover">Jobs</a> 
-      <a href="/" class="link link-hover">Press kit</a>
-    </div> 
-    <div>
-      <span class="footer-title">Contact</span> 
-      <a href="/" class="link link-hover">Terms of use</a> 
-      <a href="/" class="link link-hover">Privacy policy</a> 
-      <a href="/" class="link link-hover">Cookie policy</a>
-    </div>
-    <div>
-        <span class="footer-title">Opening Hourse</span> 
-        <a href="/" class="link link-hover">Terms of use</a> 
-        <a href="/" class="link link-hover">Privacy policy</a> 
-        <a href="/" class="link link-hover">Cookie policy</a>
-      </div>
-  </footer> 
-  <footer class="footer px-10 py-4 border-t bg-base-100 text-base-content border-base-300">
-    <div class="mx-auto">
-        <p>Copyright © 2022 - All right reserved by km602 Carwash</p>
-    </div> 
+	<div>
+		<span class="footer-title">About Us</span>
+		<a href="/" class="link link-hover">Branding</a>
+		<a href="/" class="link link-hover">Design</a>
+		<a href="/" class="link link-hover">Marketing</a>
+		<a href="/" class="link link-hover">Advertisement</a>
+		<a href="/admin" class="link link-hover">Administrator</a>
+	</div>
+	<div>
+		<span class="footer-title">Our Services</span>
+		<a href="/" class="link link-hover">About us</a>
+		<a href="/" class="link link-hover">Contact</a>
+		<a href="/" class="link link-hover">Jobs</a>
+		<a href="/" class="link link-hover">Press kit</a>
+	</div>
+	<div>
+		<span class="footer-title">Contact</span>
+		<a href="/" class="link link-hover">Terms of use</a>
+		<a href="/" class="link link-hover">Privacy policy</a>
+		<a href="/" class="link link-hover">Cookie policy</a>
+	</div>
+	<div>
+		<span class="footer-title">Opening Hourse</span>
+		<a href="/" class="link link-hover">Terms of use</a>
+		<a href="/" class="link link-hover">Privacy policy</a>
+		<a href="/" class="link link-hover">Cookie policy</a>
+	</div>
+</footer>
+<footer class="footer px-10 py-4 border-t bg-base-100 text-base-content border-base-300">
+	<div class="mx-auto">
+		<p>Copyright © 2022 - All right reserved by km602 Carwash</p>
+	</div>
 </footer>
