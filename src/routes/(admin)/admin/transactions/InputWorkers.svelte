b4072af7014c11f3561548907a797eecd3cfccc6
neko-li <erica.flores0307@gmail.com>
2022-11-15 00:42:26 +0800

    reflect the emp data to multiple select of workers

--- src/routes/(admin)/admin/transactions/InputWorkers.svelte
+++ src/routes/(admin)/admin/transactions/InputWorkers.svelte
@@ -1,10 +1,23 @@
 <script>
+    import { db } from '$lib/firebase';
+	import { collection, query, onSnapshot } from 'firebase/firestore';
+	import { onMount } from 'svelte';
+
     let isActive = false;
     export let selected =[];
 
     function toggleActive() {
         isActive = !isActive
     }
+
+    let employees = [];
+
+    onMount(() => {
+        const unsubscribe = onSnapshot(query(collection(db, 'employee')), (querySnapshot) => {
+        employees = querySnapshot.docs.map((doc) => ({id: doc.id, ...doc.data() }));
+        });
+        return () => unsubscribe();
+    })
 
 
 </script>
@@ -16,16 +29,13 @@
     </label>
       <div class="max-w-lg h-13 border border-gray-300 rounded-lg p-3 text-sm font-semibold text-gray-700" on:click|preventDefault|stopPropagation={toggleActive}>{selected.length ? selected.join(", ") : 'Pick workers'}</div>
 </div>
+
 {#if isActive}
 <div class="flex flex-col gap-3 px-2 py-3 border max-w-lg max-h-32 overflow-y-auto">
-    <label>
-    <input type="checkbox" class="checkbox rounded-none mr-3 checkbox-sm" value="Ame Watson" bind:group={selected}/> Ame Watson
-    </label>
-    <label>
-    <input type="checkbox" class="checkbox rounded-none mr-3 checkbox-sm" value="John Doe" bind:group={selected}/> John Doe
-    </label>
-    <label>
-    <input type=checkbox class="checkbox rounded-none mr-3 checkbox-sm" value="Johny" bind:group={selected}> Johny
-    </label>
+    {#each employees as emp}
+        <label>
+            <input type="checkbox" class="checkbox rounded-none mr-3 checkbox-sm" value={emp.name} bind:group={selected}/> {emp.name}
+        </label>
+    {/each}
 </div>
 {/if}
