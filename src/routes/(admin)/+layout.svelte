b687cd4c7900cd55684a645c4be04407e72ea82f
neko-li <erica.flores0307@gmail.com>
2022-11-19 19:56:57 +0800

    fix overflow admin layout

--- src/routes/(admin)/+layout.svelte
+++ src/routes/(admin)/+layout.svelte
@@ -11,33 +11,22 @@
 {#if $userStore === undefined}
 	<Loading />
 {:else if $userStore !== undefined && $userStore !== null}
-	<div class="flex min-h-screen">
-		<Sidebar />
-		<div class="min-h-screen w-4/5 bg-blue-50">
+	<div class="drawer drawer-mobile">
+		<input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+		<div class="drawer-content bg-blue-50">
+		  <!-- Page content here -->
 			<div class="w-full navbar bg-base-100">
-				<div class="flex-none lg:hidden">
-					<div class="btn btn-square btn-ghost drawer-button lg:hidden">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							class="inline-block w-6 h-6 stroke-current"
-							><path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M4 6h16M4 12h16M4 18h16"
-							/></svg
-						>
-					</div>
-				</div>
 				<div class="flex-1 px-2 mx-2 text-blue-500 font-medium text-base">
 					<span class="text-yellow-400 pr-2">km602</span>Carwash
 				</div>
 			</div>
-			<div class="card-body overflow-auto">
+			<div class="card-body h-[628px] overflow-auto">
 				<slot />
 			</div>
+		  <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+		</div> 
+		<div class="drawer-side">
+		  <Sidebar />
 		</div>
 	</div>
 {/if}
