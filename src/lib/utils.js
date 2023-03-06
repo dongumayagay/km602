83e2ce3d6228541277e2b5b8a6425b6b89b7604a
neko-li <erica.flores0307@gmail.com>
2023-03-06 16:24:53 +0800

    sending email after booking

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -92,3 +92,19 @@ export function printPDF(view){
         window.open(doc.output('bloburl'), '_blank');
 
 }
+
+
+
+export const sendEmail = async (
+	mailOptions = {
+		to: 'erica.flores0307@gmail.com',
+		subject: 'Email Subject',
+		html: '<h1>Hello World</h1>'
+	}
+) =>
+	await (
+		await fetch('/api/send-email', {
+			method: 'POST',
+			body: JSON.stringify({ mailOptions })
+		})
+	).json();
--- src/routes/(client)/book/BookingForm.svelte
+++ src/routes/(client)/book/BookingForm.svelte
@@ -8,6 +8,7 @@
 	import { db } from '$lib/firebase';
 	import { addDoc, collection } from 'firebase/firestore';
 	import { onMount } from 'svelte';
+	import { sendEmail } from '$lib/utils'; 
 
 	let booking = {
 		name: "",
@@ -48,8 +49,15 @@
 		
 
 		try {
+			let htmlStr = `Dear ${booking.name},<br><br>Thank you for booking a carwash with us. Your booking details are as follows:<br><br><b>Date: ${booking.date} </b><br><b>Time: ${booking.time}</b><br><b>Location: Purok 7, Brgy. Ariman, Gubat, Sorsogon</b><br><br>We have received your request and we will do our best to accommodate your preferred time and date. However, please note that your booking is subject to availability. We will confirm your booking within the next 24 hours.<br><br>Please wait for our confirmation email before coming to the carwash. If you do not receive a confirmation from us, please contact us as soon as possible to avoid any inconvenience.<br><br>Thank you for choosing our carwash service. We look forward to seeing you soon.<br><br>Best regards,<br><br><b>Km602 Carwash</b>`;
 			await addDoc(collection(db, 'bookings'), booking);
-			alert('booked successfully');
+			const result = await sendEmail({
+				to: booking.email,
+				subject: 'Carwash Booking Confirmation',
+				html: htmlStr
+        	});
+			console.log(JSON.stringify(result));
+			alert('Booked successfully. Please check your email');
 			location = location;
 		} catch (error) {
 			console.log(error);
